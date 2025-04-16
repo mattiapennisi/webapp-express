08268bbca65ee1fa89d66ac9b778c4afe8084363
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-16 14:07:09 +0200

    Implement show route to fetch movie details and associated reviews

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
-
 Inserire delle immagini nel progetto express e dunque nel db
-Inserire i dati di connessione al database come variabili d’ambiente
-Inserire le vostre API in controller
-Inserire le vostre rotte in un router
-Inserire un middleware per le rotte inesistenti
-Inserire un middleware per la gestione errori
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -13,7 +13,31 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    
+    const id = req.params.id
+    const movieSql = `
+        SELECT *
+        FROM movies
+        WHERE id = ?
+        `
+    const reviewsSql = `
+        SELECT *
+        FROM reviews
+        WHERE movie_id = ?
+        `
+
+    connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Movie not found' })
+
+        const movie = movieResults[0]
+
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' })
+
+            movie.reviews = reviewsResults
+            res.json(movie)
+        })
+    })
 }
 
 function store(req, res) {
@@ -21,11 +45,11 @@ function store(req, res) {
 }
 
 function update(req, res) {
-    
+
 }
 
 function modify(req, res) {
-    
+
 }
 
 function destroy(req, res) {
